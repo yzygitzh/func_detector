e5f421baf7e02d0efcfc7425d9d179230e969348
Ziyue Yang <yzylivezh@hotmail.com>
2017-01-09 17:32:02 +0800

    threshold of feature cleaning

--- scripts/func_detector_train.py
+++ scripts/func_detector_train.py
@@ -73,15 +73,26 @@ def clean_obj_text(feature_obj):
                 ret_obj[feature_key] = ret_obj[feature_key].union(
                     clean_text(feature_text, dot_split=False))
 
-    for feature_key in ["service", "activity", "provider", "receiver", "library",
-                        "plurals", "string_arrays", "strings"]:
+    for feature_key in ret_obj:
         ret_obj[feature_key] = list(ret_obj[feature_key])
 
     return ret_obj
 
 
-def build_word_bag(sample_list, config_json):
-    # ret wb["WEATHER"]["activity"] == ["weather", "rain", ...]
+def cast_sample(sample, vec_space):
+    # cast_sample["manifest"] = [0, 0, 1, ...]
+    ret_list = []
+    for word in vec_space:
+        if word in sample:
+            ret_list.append(1)
+        else:
+            ret_list.append(0)
+    return ret_list
+
+
+def build_sample_vecs(sample_list, config_json):
+    # sample_vec["WEATHER"][0]["manifest"] = ["weather", "rain", ...]
+    # vec_space["WEATHER"]["manifest"] = ["snow", "rain", ...]
 
     # select features in different dimensions by calculating mi
     # using sklearn's method
@@ -95,25 +106,58 @@ def build_word_bag(sample_list, config_json):
         "public": set(),
         "permission": set()
     }
-
     count = 0
     for sample in sample_list:
         print "%s/%s" % (str(count), str(len(sample_list)))
         count += 1
         label_set = label_set.union(set(sample["class"]))
-        for dim_name in sample:
-            if dim_name in ["class"]:
-                continue
-            elif dim_name in ["service", "activity", "library", "provider", "receiver"]:
-                dim_word_set["manifest"] = dim_word_set["manifest"].union(set(sample["manifest"]))
-            elif dim_name in ["strings", "plurals", "string_arrays"]:
-                dim_word_set["string"] = dim_word_set["string"].union(set(sample["string"]))
-            else:
-                dim_word_set[dim_name] = dim_word_set[dim_name].union(set(sample[dim_name]))
-        if count == 1000:
+        for dim_name in ["service", "activity", "library", "provider", "receiver"]:
+            dim_word_set["manifest"] = dim_word_set["manifest"].union(set(sample[dim_name]))
+        for dim_name in ["strings", "plurals", "string_arrays"]:
+            dim_word_set["string"] = dim_word_set["string"].union(set(sample[dim_name]))
+        for dim_name in ["permission", "public"]:
+            dim_word_set[dim_name] = dim_word_set[dim_name].union(set(sample[dim_name]))
+        if count == 1:
             break
 
-    # build feature vectors
+    # build list for mi calc, transform back to set later
+    dim_word_list = {}
+    for dim_name in dim_word_set:
+        dim_word_list[dim_name] = list(dim_word_set[dim_name])
+
+    # build sample vecs
+    # sample_vec["WEATHER"][0]["manifest"] = ["weather", "rain", ...]
+    sample_vec = {}
+    for label in label_set:
+        sample_vec[label] = []
+
+    for sample in sample_list:
+        print sample["class"]
+        break
+
+    for sample in sample_list:
+        vec = {
+            "manifest": set(),
+            "string": set(),
+            "public": set(),
+            "permission": set()
+        }
+        for dim_name in ["service", "activity", "library", "provider", "receiver"]:
+            vec["manifest"] = vec["manifest"].union(set(sample[dim_name]))
+        for dim_name in ["strings", "plurals", "string_arrays"]:
+            vec["string"] = vec["string"].union(set(sample[dim_name]))
+        for dim_name in ["permission", "public"]:
+            vec[dim_name] = vec[dim_name].union(set(sample[dim_name]))
+
+        for dim_name in vec:
+            vec[dim_name] = cast_sample(vec[dim_name], dim_word_list[dim_name])
+
+        for label in sample["class"]:
+            sample_vec[label].append(vec)
+        break
+
+    print sample_vec
+    # vec_space["WEATHER"]["manifest"] = ["snow", "rain", ...]
 
 
 def run(config_json_path):
@@ -127,7 +171,7 @@ def run(config_json_path):
     model_output_dir = os.path.abspath(config_json["model_output_dir"])
     mode = config_json["mode"]
 
-    if mode == "data":
+    if mode == "clean_text":
         feature_path_list = ["%s/%s" % (feature_dir, x)
                              for x in os.walk(feature_dir).next()[2]]
         count = 0
@@ -141,7 +185,7 @@ def run(config_json_path):
                     cleaned_obj = clean_obj_text(feature_obj)
                     with open("%s/%s" % (data_output_dir, feature_path.split("/")[-1]), "w") as output_file:
                         json.dump(cleaned_obj, output_file)
-    elif mode == "train":
+    elif mode == "select_feature":
         data_path_list = ["%s/%s" % (data_output_dir, x)
                           for x in os.walk(data_output_dir).next()[2]]
         print "assembling sample list..."
@@ -155,7 +199,7 @@ def run(config_json_path):
         print "%s samples read." % (len(sample_list))
         # build feature vectors
         print "building feature vectors..."
-        build_word_bag(sample_list, config_json)
+        build_sample_vecs(sample_list, config_json)
 
 def parse_args():
     """
